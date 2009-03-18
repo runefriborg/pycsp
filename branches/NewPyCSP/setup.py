c6d8018ac7bd8febb1b701f0c6a93607cba7f9b9
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2009-03-18 14:42:33 +0000

    Added distutils setup.py and necessary txt files.
    create source distribution by running
    
    python setup.py sdist

---
+++ branches/NewPyCSP/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+# 
+# see http://docs.python.org/dist/dist.html
+# 
+from setuptools import setup
+
+setup(name='pycsp',
+      version='0.5.0',
+      description='PyCSP - Python CSP Library',
+      author='John Markus Bjørndalen',
+      author_email='jmb@cs.uit.no',
+      url='http://code.google.com/p/pycsp/',
+      license='MIT',
+      packages=['pycsp'],
+      platforms=['any'],
+      )
