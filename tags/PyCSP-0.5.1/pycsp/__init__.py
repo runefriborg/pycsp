9759d0105844bf23784f88ce274ec260a6e4e6dc
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2009-04-17 08:41:24 +0000

    Tag release 0.5.1

---
+++ tags/PyCSP-0.5.1/pycsp/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+"""
+PyCSP implementation of the CSP Core functionality (Channels, Processes, PAR, ALT).
+
+Copyright (c) 2009 John Markus Bjørndalen <jmb@cs.uit.no>,
+      Brian Vinter <vinter@diku.dk>, Rune M. Friborg <runef@diku.dk>.
+See LICENSE.txt for licensing details (MIT License). 
+"""
+
+from alternation import *
+from channel import *
+from channelend import *
+from process import * 
+from guard import *
+
+version = (0,5,1, '')
+
