b7e87023790cff36b5b59c55ca1e99e286dcf74b
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2009-07-06 12:22:43 +0000

    Revised MCPi

--- trunk/examples/MCPi.py
+++ trunk/examples/MCPi.py
@@ -1,39 +1,34 @@
 from common import *
 from pycsp import *
 from random import random
-import sys
 
 @process
 def producer(job_out, bagsize, bags):
-    for i in range(bags):
-        job_out(bagsize)
-    retire(job_out)
-    
+   for i in range(bags): job_out(bagsize)
+   retire(job_out)
+
 @process
 def worker(job_in, result_out):
-    while True:
-        cnt=job_in()
-        sum = reduce(lambda x,y: x+(random()**2+random()**2<1.0), range(cnt))
-        result_out((cnt,sum))
+   while True:
+       cnt=job_in()           #Get task
+       sum = reduce(lambda x,y: x+(random()**2+random()**2<1.0), range(cnt))
+       
+       result_out((4.0*sum)/cnt)  #Forward result
 
 @process
 def consumer(result_in):
-    cnt=0
-    sum=0
-    try:
-        while True:
-            c,s=result_in()
-            cnt+=c
-            sum+=s
-    except ChannelRetireException:
-        print 4.0*sum/cnt
+   cnt=0; sum=result_in()    #Get first result
+   try:
+       while True:
+           cnt+=1
+           print sum
+           sum=(sum*cnt+result_in())/(cnt+1)    #Get result
+   except ChannelRetireException:
+       print sum            #We are done - print result
 
 jobs=Channel()
 results=Channel()
-workers=[]
 
-for i in range(10):
-    workers.append(worker(IN(jobs),OUT(results)))
-Parallel(producer(OUT(jobs),1000, 1000),
-         workers,
-         consumer(IN(results)))
+Parallel(producer(OUT(jobs),1000, 10000),
+        [worker(IN(jobs),OUT(results)) for i in range(10)],
+        consumer(IN(results)))
