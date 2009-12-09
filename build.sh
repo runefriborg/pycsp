9062738d804a86c9545d95b574b628f036c3fe80
bognolo <bognolo@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2009-12-09 08:33:48 +0000

    first test of a design for simulation in pycsp

---
+++ build.sh
@@ -0,0 +1,1 @@
+make builddeb &&  make clean && lintian -IviE --display-experimental --pedantic -L ">=wishlist" --color auto --show-overrides --checksums ../pycsp_0.6.2-0.2_i386.changes
---
+++ pycsp/simulation/sprocess.py
@@ -0,0 +1,24 @@
+"""
+Processes and execution
+
+Copyright (c) 2009 John Markus Bjoerndalen <jmb@cs.uit.no>,
+      Brian Vinter <vinter@diku.dk>, Rune M. Friborg <runef@diku.dk>.
+See LICENSE.txt for licensing details (MIT License). 
+"""
+
+from pycsp.greenlets.process import Process, process 
+from simulation import Simulation
+
+
+
+# Classes
+class Process(Process):
+    """ Process(fn, *args, **kwargs)
+    It is recommended to use the @process decorator, to create Process instances
+    See process.__doc__
+    """
+    def __init__(self, fn, *args, **kwargs):
+ 			gProcess.__init__(self,fn,args,kwargs)       
+			self.s = Simulation()
+
+    
