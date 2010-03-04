9407fe8de2028b5658a5809d8b19df5b11baa676
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2010-03-04 13:22:19 +0000

    Updated CHANGES.txt and setup files

--- setup.py
+++ setup.py
@@ -26,20 +26,26 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pycsp',
-      version='0.6.2',
+      version='0.7.0',
       description='PyCSP - Python CSP Library',
-      long_description="""
-The PyCSP library has been completely rewritten.
-
-There is no backwards compatibility with versions prior to 0.5.0.
-
-For now please look at the examples for documentation on how to use the library.""",
-      author='John Markus Bjørndalen',
-      author_email='jmb@cs.uit.no',
+      long_description=read('README.txt'),
+      keywords = "python csp concurrency communicating sequential processes",
+      author='Rune Møllegaard Friborg',
+      author_email='runef@diku.dk',
       url='http://code.google.com/p/pycsp/',
       license='MIT',
-      packages=['pycsp', 'pycsp.threads', 'pycsp.processes', 'pycsp.greenlets', 'pycsp.net'],
+      packages=['pycsp', 'pycsp.threads', 'pycsp.processes', 'pycsp.greenlets', 'pycsp.net', 'pycsp.common'],
       platforms=['any'],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",        
+        ],
       )
