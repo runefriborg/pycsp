bee2059e32f7372d1fefe3fefa61d4c1d7f681af
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2010-07-28 08:38:27 +0000

    Fixed missing replace of import

--- trunk/pycsp/processes/channelend.py
+++ trunk/pycsp/processes/channelend.py
@@ -22,7 +22,7 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-from const import *
+from pycsp.common.const import *
 
 # Exceptions
 class ChannelRetireException(Exception): 
