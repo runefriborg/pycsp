982fa74a6991bf870d630abd749bbe8fde693566
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2011-10-13 09:42:16 +0000

    Clean up

--- branches/SocketPyCSP/pycsp/socket.py
+++ branches/SocketPyCSP/pycsp/socket.py
@@ -61,161 +61,39 @@ cmd_to_str = {
     }         
 
 
-class RequestHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        """
-        Here we handle the receiving end for a connection.
-        """
-
-        # Get necessary classes
-        chanman = ChannelHomeManager() 
-        comm = Communicator()
-        sched = comm.scheduler
-
-        #print 'Starting thread'
-        # Start connection handling.
-        while True:
-            req = self.request.recv(header_size)
-            if len(req) == 0:
-                # connection broken.                
-                break
-            header = struct.unpack(header_fmt, req)
-            #print header
-            if header[HEADER_PAYLOAD_SIZE] > 0:
-                payload = pickle.loads(self.request.recv(header[HEADER_PAYLOAD_SIZE]))
-
-            reply_addr = (header[HEADER_REPLY_HOST].strip('\x00'), header[HEADER_REPLY_PORT])
-            p_id = header[HEADER_PROCESS_ID].strip('\x00')
-            cmd = header[HEADER_CMD]
-
-            print "IN:"
-            print "  C:"+ cmd_to_str[cmd]
-            print "  H:"+ str(header)
-            print "  P:"+ str(payload)
-
-            if cmd == CMD_GET_CHANNEL_HOME:
-                chan = chanman.lookup(payload)
-                if chan:
-                    print "REPLY:"
-                    print "  comm.send_payload(reply_addr, CMD_EVENT, p_id, comm.addr)"
-                    comm.send_payload(reply_addr, CMD_EVENT, p_id, comm.addr)
-                else:
-                    comm.send_event_fail(reply_addr, p_id)
-
-            elif cmd == CMD_EVENT:
-                sched.new_process_event(p_id, payload)
-
-            elif cmd == CMD_EVENT_FAIL:
-                sched.new_process_event(p_id, None)
-            else:
-                # The event is passed on to the scheduler.
-                sched.new_scheduler_event(reply_addr, p_id, cmd, payload[0], payload[1], payload[2])
+def postREAD(req):
+    
+    
+def removeREAD(req):
 
 
 
-class ServerThread(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
-
-
-class Communicator(object):
+class LockThread(threading.Thread):
+    """ Process(func, *args, **kwargs)
+    It is recommended to use the @process decorator, to create Process instances
     """
-    Singleton
-    """
-    __instance = None  # the unique instance
-    def __new__(cls, *args, **kargs):
-        return cls.getInstance(cls, *args, **kargs)
+    def __init__(self, process, cond):
+        threading.Thread.__init__(self)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        
+        # Create unique id
+        self.id = str(random.random())+str(time.time())
 
-    def __init__(self, scheduler=None):
-        if scheduler:
-            self.scheduler= scheduler
-
-    def getInstance(cls, *args, **kwargs):
-        '''Static method to have a reference to **THE UNIQUE** instance'''
-        if cls.__instance is None:
-            # Initialize **the unique** instance
-            cls.__instance = object.__new__(cls)
-            
-            if not os.environ.has_key('PYCSP_PORT'):
-                raise Exception('Error! Can not use distributed channels without the PYCSP_PORT environment variable.')
-            
-            if os.environ['PYCSP_PORT'] == 'AUTO':                
-                pycsp_port = 0
-            else:
-                pycsp_port = int(os.environ['PYCSP_PORT'])
-
-            cls.__instance.server = ServerThread(("", pycsp_port), RequestHandler)
-            cls.__instance.addr = cls.__instance.server.server_address
-
-            cls.__instance.server_thread = threading.Thread(target=cls.__instance.server.serve_forever)
-
-            # Exit the server thread when the main thread terminates
-            cls.__instance.server_thread.setDaemon(True)
-            cls.__instance.server_thread.start()
-
-            #server.shutdown()
-
-            # key = addr, value = (
-            cls.__instance.connections = {}
-
-            cls.__instance.wait_list = []
-
-        return cls.__instance
-    getInstance = classmethod(getInstance)
-
-    def send_payload(self, addr, cmd, p_id, payload):
-        pickle_payload = pickle.dumps(payload, protocol = pickle.HIGHEST_PROTOCOL)
-        header = struct.pack(header_fmt, p_id, self.addr[0], self.addr[1], cmd, len(pickle_payload))
-        self.send(addr, header, pickle_payload)
+    def run(self):
 
         
-    def send(self, addr, header, data=0):
-        """
-        addr = (host, port)
-        """
-        if not self.connections.has_key(addr):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(addr)            
-            self.connections[addr] = sock
 
-        self.connections[addr].sendall(header)
-        if data:
-            self.connections[addr].sendall(data)
+        try:
+            # Store the returned value from the process
+            self.fn(*self.args, **self.kwargs)
+        except ChannelPoisonException, e:
+            # look for channels and channel ends
+            self.__check_poison(self.args)
+            self.__check_poison(self.kwargs.values())
+        except ChannelRetireException, e:
+            # look for channel ends
+            self.__check_retire(self.args)
+            self.__check_retire(self.kwargs.values())
 
-
-    #def send_event_fail(self, addr, p_id):
-    #    header = struct.pack(header_fmt, p_id, self.addr[0], self.addr[1], CMD_EVENT_FAIL, 0, 0, 0)
-
-    def request_channel_home(self, addr, name):
-        p = self.scheduler.current
-        self.send_payload(addr, CMD_GET_CHANNEL_HOME, p.id, name)
-        self.scheduler.event_wait()
-        return p.event
-
-    def channel_join(self, home_addr, name, mode, end_id):
-        p = self.scheduler.current
-        self.send_payload(home_addr, CMD_CHANNEL_JOIN, p.id, (mode, end_id, name))
-        #self.scheduler.event_wait()
-        return
-
-    def channel_leave(self, home_addr, name, mode, end_id):
-        p = self.scheduler.current
-        self.send_payload(home_addr, CMD_CHANNEL_LEAVE, p.id, (mode, end_id, name))
-        #self.scheduler.event_wait()
-        return
-
-
-#comm = Communicator()
-#comm = Communicator()
-
-#comm.send_payload(("",int(os.environ['PYCSP_PORT'])), "Hello1")
-#comm.send_payload(("",int(os.environ['PYCSP_PORT'])), "Hello35")
-#comm.send_payload(("192.168.16.83",int(os.environ['PYCSP_PORT'])), "Externa?")
-
-#comm.send_payload(("",int(os.environ['PYCSP_PORT'])), "and old one again")
-
-
-#import time
-#time.sleep(10)
-
-
-#comm.server_thread.shutdown()
