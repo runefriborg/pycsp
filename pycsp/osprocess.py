0668f390dffa307dea757c20af62bc7935792ee7
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2011-10-13 11:01:56 +0000

    Improved OS process abstraction

--- pycsp/osprocess.py
+++ pycsp/osprocess.py
@@ -3,13 +3,35 @@ import os
 
 ENVVAL = 'PYCSP_MULTIPROCESSING'
 
-if not os.environ.has_key(ENVVAL):
-    from threading import Thread as Proc
+if os.environ.has_key(ENVVAL) and not os.environ[ENVVAL] == '':
+    from multiprocessing import Process as Proc    
+
+    def getProcName():
+        p = osprocess.current_process()
+        name = p.name
+        if name == 'MainProcess':
+            return '__mainproc__'
+        else:
+            return name
+        
 else:
-    if os.environ[ENVVAL] == '':
-        from threading import Thread as Proc
-    else:
-        from multiprocessing import Process as Proc
+    from threading import Thread as Proc
+
+    def getProcName():
+        try:
+            # compatible with Python 2.6+
+            t = osprocess.current_thread()
+            name = t.name
+        except AttributeError:
+            # compatible with Python 2.5- 
+            t = threading.currentThread()
+            name = t.getName()
+    
+        if name == 'MainThread':
+            return '__mainproc__'
+        else:
+            return name
+
 
 
 
