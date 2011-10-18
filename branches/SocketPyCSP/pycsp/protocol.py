ac7f547a9dd4e252d377be9336085c48f4343307
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2011-10-18 20:42:20 +0000

    ThreadedTCPServer approach to LockThread process

--- branches/SocketPyCSP/pycsp/protocol.py
+++ branches/SocketPyCSP/pycsp/protocol.py
@@ -35,32 +35,21 @@ import cPickle as pickle
 import struct
 
 
-# = : Native byte-order, standard size
-# 36s : string, process uuid4
-# 64s : string, ip/hostname
-# h : short, port
-# h : short, Command.
-# l : long, payload size following this header.
-HEADER_PROCESS_ID, HEADER_REPLY_HOST, HEADER_REPLY_PORT, HEADER_CMD, HEADER_PAYLOAD_SIZE = range(5)
+# Header CMDs:
+LOCKTHREAD_ACQUIRE_LOCK, LOCKTHREAD_ACCEPT_LOCK, LOCKTHREAD_NOTIFY_SUCCESS, LOCKTHREAD_POISON, LOCKTHREAD_RETIRE = range(5)
 
-header_fmt = "=36s64shhl"
+# Header fields:
+H_CMD, H_ID, H_MSG_SIZE = range(3)
+
+# = : Native byte-order, standard size
+# h : short, CMD
+# l : long, ID
+# l : long, payload size following this header
+header_fmt = "=hll"
 header_size = struct.calcsize(header_fmt)
 
-# Commands:
-CMD_GET_CHANNEL_HOME, CMD_EVENT, CMD_EVENT_FAIL, CMD_CHANNEL_JOIN, CMD_CHANNEL_LEAVE = range(5)
-
-
-
-cmd_to_str = {
-    CMD_GET_CHANNEL_HOME:"GET_CHANNEL_HOME",
-    CMD_EVENT:"EVENT",
-    CMD_EVENT_FAIL:"EVENT_FAIL",
-    CMD_CHANNEL_JOIN:"CHANNEL_JOIN",
-    CMD_CHANNEL_LEAVE:"CHANNEL_LEAVE"
-    }         
-
-
 def join_reader(channel):
+    channel.channelhome
     pass
     #channel.connect
 
@@ -89,13 +78,112 @@ def remote_write(channel):
 def compile_header(cmd, id, arg):
     pass
 
+class LockVal(object):
+    def __init__(self):
+        self.id = None
+        self.counter = 0
+
+    def acquire(self, id):
+        if self.counter == 0:
+            self.id = id
+
+        if self.id == id:
+            self.counter += 1
+
+            return True
+        else:
+            return False
+
+    def release(self, id):
+        if self.id != id or self.counter == 0:
+            raise Exception("Should never release a lock, which is not acquired!")
+        
+        self.counter -= 1
+
+        if self.counter == 0:
+            return True
+        else:
+            return False
+    
+
 class LockThread(threading.Thread):
     def __init__(self, process, cond):
         threading.Thread.__init__(self)
 
-        class LockThreadHandler(SocketServer.BaseRequestHandler):
+        class Handler(SocketServer.BaseRequestHandler):
+            def setup(self):
+                self.lock = LockVal()
+                self.lock_acquire_queue = []
+
+            def handle(self):
+                compiled_header = self.request.recv(header_size)
+                if len(compiled_header) == 0:
+                    # connection broken.
+                    raise Exception("connection broken")
+
+                header = struct.unpack(header_fmt, compiled_header)
+
+                if header[H_CMD] == LOCKTHREAD_ACQUIRE_LOCK:
+                    if self.lock.acquire(header[H_ID]):
+                        # Send reply
+                        self.request.sendall(compile_header(LOCKTHREAD_ACCEPT_LOCK, header[H_ID], process.state))
+                    else:
+                        self.lock_acquire_queue.append(header)
+                        
+
+                if self.lock[0] == header[H_ID] and self.lock[1] > 0:
+                    if header[H_CMD] == LOCKTHREAD_RELEASE_LOCK:
+                        
+                        if self.lock.release(header[H_ID]):
+                            # The lock is no longer acquired by anyone
+                            if self.lock_acquire_queue:
+                                header = self.lock_acquire_queue.pop(0)
+                                self.lock.acquire(header[H_ID]):
+                                # Send reply
+                                s = open(
+
+                        
+
+                    if header[H_CMD] == LOCKTHREAD_NOTIFY_SUCCESS:
+                        cond.acquire()
+                        process.state = SUCCESS
+                        data = pickle.loads(self.request.recv(header[H_MSG_SIZE]))
+                        process.result_ch = data['channel']
+                        process.result_msg = data['msg']
+                        cond.notifyAll()
+                        cond.release()
+
+                    if header[H_CMD] == LOCKTHREAD_POISON:
+                        cond.acquire()
+                        process.state = POISON
+                        cond.notifyAll()
+                        cond.release()
+
+                    if header[H_CMD] == LOCKTHREAD_RETIRE:
+                        cond.acquire()
+                        process.state = RETIRE
+                        cond.notifyAll()
+                        cond.release()
+           
+        self.server = SocketServer.TCPServer(("localhost", 0), Handler)
+        self.address = self.server.server_address
+
+    def run(self):
+        server.serve_forever()
+
+    def shutdown(self):
+        server.shutdown()
+
+
+
+class ChannelHomeThread(threading.Thread):
+    def __init__(self, process, cond):
+        threading.Thread.__init__(self)
+
+        class Handler(SocketServer.BaseRequestHandler):
             def setup(self):
                 self.lock = (None, 0)
+                self.lock_acquire_queue = []
 
             def handle(self):
                                 
@@ -114,11 +202,14 @@ class LockThread(threading.Thread):
                         self.lock[1] += 1
                         
                         # Send reply
-                        self.request.send(compile_header(LOCKTHREAD_ACCEPT_LOCK, header[H_ID], process.state)
+                        self.request.sendall(compile_header(LOCKTHREAD_ACCEPT_LOCK, header[H_ID], process.state)
 
                 if self.lock[0] == header[H_ID] and self.lock[1] > 0:
                     if header[H_CMD] == LOCKTHREAD_RELEASE_LOCK:
                         self.lock[1] -= 1
+                        if self.lock[1] == 0:
+                                                 
+                                                 
 
                     if header[H_CMD] == LOCKTHREAD_NOTIFY_SUCCESS:
                         cond.acquire()
@@ -143,19 +234,7 @@ class LockThread(threading.Thread):
 
                 
 
-
-
-                    
-                    
-                print "Welcome"
-                # self.request is the TCP socket connected to the client
-                self.data = self.request.recv(1024).strip()
-                print "%s wrote:" % self.client_address[0]
-                print self.data
-                # just send back the same data, but upper-cased
-                self.request.send(self.data.upper())
-
-        self.server = SocketServer.TCPServer(("localhost", 0), LockThreadHandler)
+        self.server = SocketServer.TCPServer(("localhost", 0), Handler)
         self.address = self.server.server_address
 
     def run(self):
@@ -163,3 +242,36 @@ class LockThread(threading.Thread):
 
     def shutdown(self):
         server.shutdown()
+
+
+
+class InitChannelHome(object):
+    """
+    Singleton
+    """
+    __instance = None  # the unique instance
+    def __new__(cls, *args, **kargs):
+        return cls.getInstance(cls, *args, **kargs)
+
+    def __init__(self):
+        pass
+    
+    def getInstance(cls, *args, **kwargs):
+        '''Static method to have a reference to **THE UNIQUE** instance'''
+        if cls.__instance is None:
+            # Initialize **the unique** instance
+            cls.__instance = object.__new__(cls)
+            
+            cls.__instance.chan_home_db = {}
+            
+        return cls.__instance
+    getInstance = classmethod(getInstance)
+
+    def request(self, chan_id):
+        if self.chan_home_db.has_key(chan_id):
+            return self.chan_home_db[chan_id]
+        else:
+            return None
+
+    def add(self, chan_id, chan_home):
+        self.chan_home_db['chan_id'] = chan_home
