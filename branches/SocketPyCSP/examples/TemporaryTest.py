e94bbe30f01e9e598aa65631d113c787771ac439
runedrengen <runedrengen@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2012-03-12 12:08:11 +0000

    Added two small tests to SocketPyCSP

---
+++ branches/SocketPyCSP/examples/TemporaryTest.py
@@ -0,0 +1,23 @@
+from pycsp_import import *
+
+
+@process
+def fisk(cin):
+    cin()
+    poison(cin)
+
+@process
+def fisk2(cout):
+    for i in range(4):
+        cout(i)
+
+A = Channel()  
+print "Channel address: ", A.address
+
+Parallel(fisk(A.reader()) * 4,
+         fisk2(A.writer()))
+
+print "The End"
+
+close(A)
+shutdown()
