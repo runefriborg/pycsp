aca048f528960aeb008c84fce33a6c13351b507b
rune.m.friborg <rune.m.friborg@eca1dd04-0285-11de-9d1a-b59b2e1864b6>
2012-11-15 21:32:10 +0000

    Removed pycsp.greenlets from trunk/base distribution

--- trunk/base/pycsp/greenlets/__init__.py
+++ trunk/base/pycsp/greenlets/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
 """
-PyCSP.greenlets implementation of the CSP Core functionality (Channels, Processes, PAR, ALT).
+Placeholder for functionality available in the extras distribution for pycsp.
+See http://code.google.com/p/pycsp
 
-Copyright (c) 2009 John Markus Bjørndalen <jmb@cs.uit.no>,
-      Brian Vinter <vinter@diku.dk>, Rune M. Friborg <runef@diku.dk>
+Copyright (c) 2009 John Markus Bjoerndalen <jmb@cs.uit.no>,
+      Brian Vinter <vinter@diku.dk>, Rune M. Friborg <runef@diku.dk>.
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
 "Software"), to deal in the Software without restriction, including
@@ -24,74 +24,4 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-# Test for Greenlets
-import sys
-try: from greenlet import greenlet
-except ImportError, e:
-    try: from py.magic import greenlet
-    except ImportError, e: 
-        sys.stderr.write("PyCSP.greenlets requires the greenlet module, recommended version is 0.2 and is\navailable from http://pypi.python.org/pypi/greenlet/.\n\n")
-        raise ImportError(e)
-
-# Imports
-from scheduling import Io, io
-from guard import Skip, Timeout, SkipGuard, TimeoutGuard
-from alternation import choice, Alternation
-from altselect import FairSelect, AltSelect, InputGuard, OutputGuard
-from channel import Channel
-from channelend import retire, poison
-from process import Process, process, Sequence, Parallel, Spawn, current_process_id
-from exceptions import ChannelPoisonException, ChannelRetireException
-from compat import *
-
-version = (0,9,0, 'greenlets')
-
-# Set current implementation
-import pycsp.current
-pycsp.current.version = version
-pycsp.current.trace = False
-
-pycsp.current.Skip = Skip
-pycsp.current.Timeout = Timeout
-pycsp.current.SkipGuard = SkipGuard
-pycsp.current.TimeoutGuard = TimeoutGuard
-pycsp.current.choice = choice
-pycsp.current.Alternation = Alternation
-pycsp.current.Channel = Channel
-pycsp.current.ChannelPoisonException = ChannelPoisonException
-pycsp.current.ChannelRetireException = ChannelRetireException
-pycsp.current.ChannelSocketException = ChannelSocketException
-pycsp.current.FatalException = FatalException
-pycsp.current.retire = retire
-pycsp.current.poison = poison
-pycsp.current.io = io
-pycsp.current.Process = Process
-pycsp.current.process = process
-pycsp.current.Sequence = Sequence
-pycsp.current.Parallel = Parallel
-pycsp.current.Spawn = Spawn
-pycsp.current.current_process_id = current_process_id
-pycsp.current.FairSelect = FairSelect
-pycsp.current.AltSelect = AltSelect
-pycsp.current.InputGuard = InputGuard
-pycsp.current.OutputGuard = OutputGuard
-pycsp.current.shutdown = shutdown
-
-def test_suite():
-    import unittest
-    import doctest
-    import scheduling, guard, alternation, channel, channelend, process, buffer
-
-    suite = unittest.TestSuite()
-    for mod in scheduling, guard, alternation, channel, channelend, process, buffer:
-        suite.addTest(doctest.DocTestSuite(mod))
-    suite.addTest(doctest.DocTestSuite())
-    return suite
-
-# Run tests
-if __name__ == '__main__':
-    import unittest
-
-    suite = test_suite()
-    runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite)
+raise Exception("pycsp.greenlets is not available in the base distribution. This functionality can be acquired by installing the extras distribution from http://code.google.com/p/pycsp")
